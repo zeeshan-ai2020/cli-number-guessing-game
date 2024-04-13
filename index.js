796ef3a8013dcac6584b5804e7627ec7b1ee8ac2
zeeshan-ai2020 <zeeshanai2020@gmail.com>
2024-04-13 02:05:06 -0700

    created number guessing game

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+// console.log(randomNumber);
+const answers = await inquirer.prompt([{
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please Guess a Number between 1-6:",
+    },
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("Congratulatios! You Guessed Right Number.");
+}
+else {
+    console.log("You Guessed Wrong Number.");
+}
