cf60002582554c6365cd383af6dcad2a1629bc7a
zeeshan-ai2020 <zeeshanai2020@gmail.com>
2024-04-13 02:20:04 -0700

    Added a console

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@
 import inquirer from "inquirer";
 
 const randomNumber = Math.floor(Math.random() *6 + 1);
-// console.log(randomNumber);
+console.log("Welcome to Number Guessing G");
 const answers = await inquirer.prompt([{
     name: "userGuessedNumber",
     type: "number",
